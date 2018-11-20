4e1f35e068213a0198772b0ae7bc992e7f4a18bc
Ken.Liu <liukun@aixuedai.com>
2018-11-20 21:35:24 +0800

    基本的路由模式

--- WeBuy/common/router.js
+++ WeBuy/common/router.js
@@ -1,11 +1,39 @@
+var routerMap = {
+	"wb://login/login": "/pages/login/login",
+	"wb://cart/cart": "/pages/cart/cart",
+	"wb://account/setting": "/pages/account/setting"
+};
+
 export default {
 	install(Vue,options) {
 		Vue.prototype.router = function (url, callback) {
-			//这里要公共处理一下路由的跳转
-			uni.showToast({
-				title: url,
-				icon: 'none'
-			});
+			var dispatch = getDispatchUrl(url);
+			
+			if (dispatch.length > 0) {
+				uni.navigateTo({
+					url: dispatch,
+					success: res => {},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
 		}
 	}
+}
+
+function getDispatchUrl(url) {
+	console.log(routerMap[url.split("?")[0]]);
+	
+	var array = url.split("?");
+	var path = routerMap[array[0]];
+	
+	if (typeof path == 'string' && path.length > 0) {
+		if (array.length > 0) {
+			return path + '?' + array[1];
+		}
+		return path;
+	} else {
+		return '';
+	}
+	return routerMap[url.split("?")[0]];
 }
